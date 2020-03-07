ea1c0e37d6a367b9ae5bc428bb3a61d26779cc47
ChArtemDm <artem74ru@hotmail.com>
2020-03-07 21:41:37 +0500

    revorked for games

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import readlineSync from 'readline-sync';
+import evenGame from './games/even.js';
+import calcGame from './games/calc.js';
 
-export default () => {
+export default (game) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
+  // eslint-disable-next-line consistent-return
+  const play = (message) => {
+    switch (message) {
+      case 'brain-games':
+        break;
+      case 'evenGame':
+        return evenGame(name);
+      case 'calcGame':
+        return calcGame(name);
+      default:
+        throw new Error(`Unknown game '${message}'`);
+    }
+  };
+  return play(game);
 };
