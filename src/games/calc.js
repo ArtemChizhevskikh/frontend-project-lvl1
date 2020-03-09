316153b90fb63a4579665b448699748a568f7d29
ChArtemDm <artem74ru@hotmail.com>
2020-03-09 19:03:16 +0500

    code reworked

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,37 +1,24 @@
-import readlineSync from 'readline-sync';
 import randomNumber from '../random.js';
 
-export default (name) => {
-  console.log('What is the result of the expression?');
-  let rightAnswers = 0;
-  const coll = ['+', '-', '*'];
-  do {
-    const number1 = randomNumber(1, 20);
-    const number2 = randomNumber(1, 20);
-    const randomOperator = coll[randomNumber(1, 3) - 1];
-    const randomOperation = (num1, num2, message) => {
-      switch (message) {
-        case '+':
-          return num1 + num2;
-        case '-':
-          return num1 - num2;
-        case '*':
-          return num1 * num2;
-        default:
-          throw new Error(`Unknown operator '${message}'`);
-      }
-    };
-    const rightAnswer = randomOperation(number1, number2, randomOperator);
-    console.log(`Question: ${number1} ${randomOperator} ${number2}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === rightAnswer.toString()) {
-      console.log('Correct!');
-      rightAnswers += 1;
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}."`);
-      console.log(`Let's try again, ${name}`);
-      rightAnswers = 0;
+export default () => {
+  const operators = ['+', '-', '*'];
+  const number1 = randomNumber(1, 20);
+  const number2 = randomNumber(1, 20);
+  const randomOperator = operators[randomNumber(1, 3) - 1];
+  const randomOperation = (num1, num2, message) => {
+    switch (message) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      default:
+        throw new Error(`Unknown operator '${message}'`);
     }
-  } while (rightAnswers < 3);
-  console.log(`Congratulations, ${name}!`);
+  };
+  const rightAnswer = `${randomOperation(number1, number2, randomOperator)}`;
+  const question = `${number1} ${randomOperator} ${number2}`;
+  const coll = { rightAnswer, question };
+  return coll;
 };
