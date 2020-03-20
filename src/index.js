5e9d88bc87975dce5bb5472454430e9f05563d52
ArtemChizhevskikh <artem74ru@hotmail.com>
2020-03-20 18:02:33 +0500

    refactor rename functions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import readlineSync from 'readline-sync';
 
 
-export default (rules, func) => {
+export default (gameDescription, getGameData) => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(rules);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log(gameDescription);
+
   let rightAnswers = 0;
-  const cycle = () => {
-    const coll = func();
-    console.log(`Question: ${coll.question}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer !== coll.rightAnswer) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${coll.rightAnswer}".`);
-      console.log(`Let's try again, ${name}`);
-      rightAnswers = 0;
-      return;
-    }
-    console.log('Correct!');
-    rightAnswers += 1;
-  };
   while (rightAnswers < 3) {
-    cycle();
+    const gameData = getGameData();
+    console.log(`Question: ${gameData.question}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer !== gameData.rightAnswer) {
+      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${gameData.rightAnswer}".`);
+      console.log(`Let's try again, ${playerName}`);
+      rightAnswers = 0;
+    } else {
+      console.log('Correct!');
+      rightAnswers += 1;
+    }
   }
-  console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${playerName}!`);
 };
